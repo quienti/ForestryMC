0bb9f2a991d291a26c41a1902c0ce58a46065f07
Nedelosk <nedelosk@gmail.com>
2020-02-15 11:00:31 +0100

    Fix crash caused by invalid ore meta value

--- src/main/java/forestry/core/blocks/BlockResourceOre.java
+++ src/main/java/forestry/core/blocks/BlockResourceOre.java
@@ -10,35 +10,120 @@
  ******************************************************************************/
 package forestry.core.blocks;
 
-import java.util.Random;
-
+import forestry.api.core.IItemModelRegister;
+import forestry.api.core.IModelManager;
+import forestry.core.CreativeTabForestry;
+import forestry.core.ModuleCore;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.IWorldReader;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-import net.minecraftforge.common.ToolType;
+import java.util.Random;
 
-public class BlockResourceOre extends Block {
-	private final EnumResourceType type;
+public class BlockResourceOre extends Block implements IItemModelRegister, IBlockWithMeta {
+	public static final PropertyEnum<EnumResourceType> ORE_RESOURCES = PropertyEnum.create("resource", EnumResourceType.class, input -> input != null && input.hasOre());
 
-	public BlockResourceOre(EnumResourceType type) {
-		super(Block.Properties.create(Material.ROCK)
-			.hardnessAndResistance(3f, 5f)
-			.harvestTool(ToolType.PICKAXE)
-			.harvestLevel(1));
-		this.type = type;
+	public BlockResourceOre() {
+		super(Material.ROCK);
+		setHardness(3F);
+		setResistance(5F);
+		setCreativeTab(CreativeTabForestry.tabForestry);
+		setDefaultState(this.blockState.getBaseState().withProperty(ORE_RESOURCES, EnumResourceType.APATITE));
 	}
 
 	@Override
-	public int getExpDrop(BlockState state, IWorldReader reader, BlockPos pos, int fortune, int silktouch) {
-		Random rand = reader instanceof World ? ((World) reader).rand : new Random();
-		if (type == EnumResourceType.APATITE) {
-			return MathHelper.nextInt(rand, 1, 4);
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, ORE_RESOURCES);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(ORE_RESOURCES).getMeta();
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(ORE_RESOURCES, EnumResourceType.VALUES[meta]);
+	}
+
+	@Override
+	public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
+		Random rand = world instanceof World ? ((World) world).rand : new Random();
+		if (state.getValue(ORE_RESOURCES) == EnumResourceType.APATITE) {
+			return MathHelper.getInt(rand, 1, 4);
 		}
-		return super.getExpDrop(state, reader, pos, fortune, silktouch);
+		return super.getExpDrop(state, world, pos, fortune);
+	}
+
+	@Override
+	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+		EnumResourceType type = state.getValue(ORE_RESOURCES);
+		switch (type) {
+			case APATITE: {
+				int fortuneModifier = RANDOM.nextInt(fortune + 2) - 1;
+				if (fortuneModifier < 0) {
+					fortuneModifier = 0;
+				}
+
+				int amount = (2 + RANDOM.nextInt(5)) * (fortuneModifier + 1);
+				if (amount > 0) {
+					drops.add(ModuleCore.getItems().apatite.getItemStack(amount));
+				}
+				break;
+			}
+			case COPPER:
+			case TIN: {
+				drops.add(new ItemStack(this, 1, damageDropped(state)));
+				break;
+			}
+			default:
+				break;
+		}
+	}
+
+	@Override
+	public int damageDropped(IBlockState state) {
+		return getMetaFromState(state);
+	}
+
+	@Override
+	public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
+		for (EnumResourceType resourceType : ORE_RESOURCES.getAllowedValues()) {
+			list.add(get(resourceType, 1));
+		}
+	}
+
+	/* MODLES */
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModel(Item item, IModelManager manager) {
+		manager.registerItemModel(item, 0, "ores/apatite");
+		manager.registerItemModel(item, 1, "ores/copper");
+		manager.registerItemModel(item, 2, "ores/tin");
+	}
+
+	public ItemStack get(EnumResourceType type, int amount) {
+		return new ItemStack(this, amount, type.getMeta());
+	}
+
+	@Override
+	public String getNameFromMeta(int meta) {
+		if (meta < 0 || meta >= EnumResourceType.BRONZE.getMeta()) {
+			return "Invalid Item Meta";
+		}
+		EnumResourceType resourceType = getStateFromMeta(meta).getValue(ORE_RESOURCES);
+		return resourceType.getName();
 	}
 }
