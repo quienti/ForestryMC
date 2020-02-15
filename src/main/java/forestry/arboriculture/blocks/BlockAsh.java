1cb5b758d78fcbfc104c4d932aaf5649db842c3b
Nedelosk <nedelosk@gmail.com>
2020-02-15 11:21:32 +0100

    Fix crash caused by invalid ore meta value

--- src/main/java/forestry/arboriculture/blocks/BlockAsh.java
+++ src/main/java/forestry/arboriculture/blocks/BlockAsh.java
@@ -1,45 +1,102 @@
 package forestry.arboriculture.blocks;
 
+import forestry.api.core.IItemModelRegister;
+import forestry.api.core.IModelManager;
+import forestry.api.core.IStateMapperRegister;
+import forestry.core.ModuleCore;
+import forestry.core.config.Constants;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.EnumPushReaction;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
-import net.minecraft.block.material.PushReaction;
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.StateContainer;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-import net.minecraftforge.common.ToolType;
+import java.util.Random;
 
-public class BlockAsh extends Block {
+public class BlockAsh extends Block implements IStateMapperRegister, IItemModelRegister {
 
-	public static final IntegerProperty AMOUNT = IntegerProperty.create("amount", 0, 63);
+	public static final PropertyInteger AMOUNT = PropertyInteger.create("amount", 0, 15);
 
-	public BlockAsh() {
-		super(Block.Properties.create(Material.EARTH, MaterialColor.BLACK).sound(SoundType.SAND).harvestTool(ToolType.SHOVEL).harvestLevel(0));
+	private final int startAmount;
+
+	public BlockAsh(int startAmount) {
+		super(Material.GROUND, MapColor.BLACK);
+		setSoundType(SoundType.SAND);
+		setHarvestLevel("shovel", 0);
+		this.startAmount = startAmount;
 	}
 
 	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(AMOUNT);
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, AMOUNT);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModel(Item item, IModelManager manager) {
+		for (int i = 0; i < 16; i++) {
+			manager.registerItemModel(item, i, "ash_block");
+		}
 	}
 
 	@Override
-	public PushReaction getPushReaction(BlockState state) {
-		return PushReaction.DESTROY;
+	public int damageDropped(IBlockState state) {
+		return getMetaFromState(state);
 	}
 
-	//TODO maybe loot table
-	//	@Override
-	//	public void getDrops(NonNullList<ItemStack> drops, IBlockReader world, BlockPos pos, BlockState state, int fortune) {
-	//		Random rand = world instanceof World ? ((World) world).rand : new Random();
-	//		int amount = startAmount + state.get(AMOUNT);
-	//		if (amount > 0) {
-	//			if (fortune > 0) {
-	//				amount += rand.nextInt(1 + fortune);
-	//			}
-	//			drops.add(new ItemStack(Items.CHARCOAL, amount));
-	//			drops.add(new ItemStack(ModuleCore.getItems().ash, 1 + rand.nextInt(amount / 4)));
-	//		}
-	//	}
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(AMOUNT);
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(AMOUNT, meta);
+	}
+
+	@Override
+	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+		Random rand = world instanceof World ? ((World) world).rand : new Random();
+		int amount = startAmount + state.getValue(AMOUNT);
+		if (amount > 0) {
+			if (fortune > 0) {
+				amount += rand.nextInt(1 + fortune);
+			}
+			drops.add(new ItemStack(Items.COAL, amount, 1));
+			drops.add(new ItemStack(ModuleCore.getItems().ash, 1 + rand.nextInt(Math.max(amount / 4, 1))));
+		}
+	}
+
+    @Override
+    public EnumPushReaction getPushReaction(IBlockState state) {
+        return EnumPushReaction.DESTROY;
+    }
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerStateMapper() {
+		ModelLoader.setCustomStateMapper(this, new StateMapperBase() {
+			@Override
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+				return new ModelResourceLocation(Constants.MOD_ID + ":ash_block", "normal");
+			}
+		});
+	}
+
+
 }
