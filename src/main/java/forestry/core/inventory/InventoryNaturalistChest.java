b3b4b4cf4b14a35cc1615e492bf4d25057890cb4
Nedelosk <nedelosk@gmail.com>
2020-03-03 21:30:34 +0100

    Fix #2314 Aborist's chest should be able to accept any sapling types

--- src/main/java/forestry/core/inventory/InventoryNaturalistChest.java
+++ src/main/java/forestry/core/inventory/InventoryNaturalistChest.java
@@ -11,26 +11,47 @@
 package forestry.core.inventory;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Direction;
+import net.minecraft.util.EnumFacing;
 
-import forestry.api.genetics.IForestrySpeciesRoot;
+import net.minecraftforge.oredict.OreDictionary;
+
+import forestry.api.arboriculture.TreeManager;
+import forestry.api.genetics.ISpeciesRoot;
 import forestry.core.tiles.TileNaturalistChest;
+import forestry.core.utils.OreDictUtil;
 
 public class InventoryNaturalistChest extends InventoryAdapterTile<TileNaturalistChest> {
-	private final IForestrySpeciesRoot speciesRoot;
+	public static final int treeSaplingId = OreDictionary.getOreID(OreDictUtil.TREE_SAPLING);
 
-	public InventoryNaturalistChest(TileNaturalistChest tile, IForestrySpeciesRoot speciesRoot) {
+	private final ISpeciesRoot speciesRoot;
+
+	public InventoryNaturalistChest(TileNaturalistChest tile, ISpeciesRoot speciesRoot) {
 		super(tile, 125, "Items");
 		this.speciesRoot = speciesRoot;
 	}
 
 	@Override
 	public boolean canSlotAccept(int slotIndex, ItemStack itemstack) {
-		return speciesRoot.isMember(itemstack);
+		if (itemstack.isEmpty()) {
+			return false;
+		}
+		if (speciesRoot.isMember(itemstack)) {
+			return true;
+		}
+		if (speciesRoot != TreeManager.treeRoot) {
+			return false;
+		}
+		int[] oreIds = OreDictionary.getOreIDs(itemstack);
+		for (int oreId : oreIds) {
+			if (oreId == treeSaplingId) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
-	public boolean canExtractItem(int slotIndex, ItemStack stack, Direction side) {
+	public boolean canExtractItem(int slotIndex, ItemStack stack, EnumFacing side) {
 		return true;
 	}
 }
